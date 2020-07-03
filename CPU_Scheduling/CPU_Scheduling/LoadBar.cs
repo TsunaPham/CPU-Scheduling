e637ecc0dbf4eb0dd3ca9246d56579b6ff4e4a57
cagood123 <65656352+cagood123@users.noreply.github.com>
2020-07-03 20:47:46 +0700

    Add files via upload

---
+++ CPU_Scheduling/CPU_Scheduling/LoadBar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPU_Scheduling
+{
+    class LoadBar
+    {
+        public ProgressBar b1 = new ProgressBar();
+        public int Max { set; get; }
+        public void load()
+        {
+            Timer T = new Timer();
+            
+            b1.Maximum = Max;
+            b1.Width = 25*Max;
+            T.Tick += new EventHandler(Loadpro);
+            T.Interval = 1000;
+            T.Start();
+        }
+        private void Loadpro(object sender, EventArgs e)
+        {
+            if (b1.Value !=Max)
+            {
+                b1.Value++;
+            }
+            
+        }
+    }
+}
